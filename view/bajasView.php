15898aef5b6503ceefff6bfd8aead427f93020c4
Frikortreat <87457741+Frikortreat@users.noreply.github.com>
2023-05-02 10:22:53 -0500

    Update bajasView.php
    Vista de Bajas en modal

--- view/bajasView.php
+++ view/bajasView.php
@@ -1,43 +1,96 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bajas</title>
-</head>
-<body>
-   <form  method="post">
-    <div class="form-group">
-        <label for="motivo">Motivo:</label>
-        <input type="text" class="form-control" id="motivo" name="motivo">
-        <label for="descripcion">Descripcion:</label>
-        <input type="text" class="form-control" id="descripcion" name="descripcion">
-        <label for="fecha">Fecha:</label>
-        <input type="date" class="form-control" id="fecha" name="fecha">
+<?php
+include('../templates/cabecera.php');
+?>
 
-    <button type="submit" class="btn btn-primary">Enviar</button>
-    <input type="hidden" name="csrf_token" value="${csrf_token}"/>
-    </form>
-   
-    <table>
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Motivo</th>
-                <th>Descripcion</th>
-                <th>Fecha</th>
-                
-            </tr>
-        </thead>
-        <tbody id="tbBajas">
-            <tr>
-            
-                
-            </tr>
-        </tbody>
-    </table>
 
-</body>
-<script src='../js/bajas.js'></script>
-</html>
+<!-- Modal Roles -->
+<div class="modal fade" id="bajasModal" tabindex="-1" aria-labelledby="rolesModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Editar Roles</h5>
+                <button type="button" class="btn-close" data-coreui-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form id="formBajas">
+                <div class="modal-body">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1" class="mb-2">Equipo:</label>
+                                <div id="Equipo"></div>
+                                <input type="text" class="form-control mb-2" id="codigoEquipo" name="codigoEquipo" placeholder="Ingrese código">
+                                <label for="exampleInputEmail1" class="mb-2">Área:</label>
+                                <select class="form-select" aria-label="Roles">
+                                    <option selected>Seleccione el Área</option>
+                                    <option value="1">Área 1</option>
+                                    <option value="2">Área 2</option>
+                                    <option value="3">Área 3</option>
+                                </select>
+                                <label for="exampleInputEmail1" class="mb-2">Motivo:</label>
+                                <select class="form-select" aria-label="Roles">
+                                    <option selected>Seleccione el Motivo</option>
+                                    <option value="1">Motivo 1</option>
+                                    <option value="2">Motivo 2</option>
+                                    <option value="3">Motivo 3</option>
+                                    <option value="4">Otros</option>
+                                </select>
+
+                                <div id="alerta"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-coreui-dismiss="modal">Cerrar</button>
+                    <button type="submit" class="btn btn-primary">Guardar</button>
+
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- /*Fin del modal roles*/ -->
+
+<!--Contenido-->
+<div class="col-md-12 col-lg-12 ">
+    <div class="row ">
+        <div class="col-xs-1-12">
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title mb-4">Lista Bajas <button type="button" class="btn btn-outline-primary" data-coreui-toggle="modal" data-coreui-target="#bajasModal"><strong>Añadir</strong></button></h3>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Buscar Área" aria-label="Recipient's username" aria-describedby="button-addon2" name="txtBuscarRoles" id="txtBuscarRoles">
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-1-12">
+            <div class="card">
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col"><strong>#</strong></th>
+                                <th scope="col"><strong>Equipo</strong></th>
+                                <th scope="col"><strong>Area</strong></th>
+                                <th scope="col"><strong>Motivo</strong></th>
+                                <th scope="col"><strong>Fecha</strong></th>
+                                <th scope="col"><strong>Opciones</strong></th>
+                            </tr>
+                        </thead>
+                        <tbody id="tbRoles">
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/Contenido-->
+
+<script src="../js/areaAjax.js"></script>
+<?php
+include '../templates/footer.php';
+?>
