012dcc74c21a69a43af63b00fd432bf0f57a2ab1
Frikortreat <87457741+Frikortreat@users.noreply.github.com>
2023-05-05 12:54:39 -0500

    Update bajasView.php

--- view/bajasView.php
+++ view/bajasView.php
@@ -81,7 +81,7 @@ include('../templates/cabecera.php');
                                             <option value="10">10</option>
                                             <option value="15">15</option>
                                         </select>
-                                        Entradas</label>
+                                        Entradasss</label>
                                 </div>
                             </div>
                             <div class="col-lg-2 col-sm-4 text-end">
