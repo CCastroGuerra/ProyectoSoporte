e8f0e85df97c11381fdbffee7ddb290a80be79a0
Frikortreat <87457741+Frikortreat@users.noreply.github.com>
2023-05-11 12:49:58 -0500

    guardar submit

--- view/personalView.php
+++ view/personalView.php
@@ -41,7 +41,7 @@ include('../templates/cabecera.php');
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary btncerrar" data-coreui-dismiss="modal">Cerrar</button>
-                    <button type="button" class="btn btn-primary" data-coreui-dismiss="modal">Guardar</button>
+                    <button type="submit" class="btn btn-primary" data-coreui-dismiss="modal">Guardar</button>
                 </div>
             </form>
         </div>
