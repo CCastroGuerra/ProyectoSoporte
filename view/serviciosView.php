c930923ec12254d84b4dfa4ed94ce411e1d4b9c0
Frikortreat <87457741+Frikortreat@users.noreply.github.com>
2023-05-16 12:57:30 -0500

    Update serviciosView.php

--- view/serviciosView.php
+++ view/serviciosView.php
@@ -8,7 +8,7 @@ include('../templates/cabecera.php');
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Editar Servicio</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Agregar Servicio</h5>
         <button type="button" class="btn-close" data-coreui-dismiss="modal" aria-label="Close"></button>
       </div>
       <form id="formServicio">
