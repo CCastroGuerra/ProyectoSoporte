b4072db252b735e4e3823c890a9d0199fbfeb2f0
CCastroGuerra <122035376+CCastroGuerra@users.noreply.github.com>
2023-05-26 12:49:21 -0500

    update de segundos de sesion

--- templates/sesion.php
+++ templates/sesion.php
@@ -10,7 +10,7 @@
    // $tipo_usuario = $_SESSION['tipo_usuario'];
    
    // Establecer tiempo de vida de la sesión en segundos
-   $inactividad = 3000;
+   $inactividad = 600;
    // Comprobar si $_SESSION["timeout"] está establecida
    if(isset($_SESSION["timeout"])){
        // Calcular el tiempo de vida de la sesión (TTL = Time To Live)
