f5ebe1410a7e9a0fde75461ad23eeb7d11f91f28
Frikortreat <87457741+Frikortreat@users.noreply.github.com>
2023-06-02 11:48:25 -0500

    Update productosView.php

--- view/productosView.php
+++ view/productosView.php
@@ -32,8 +32,8 @@ include('../templates/cabecera.php');
                         <table class="table table-hover align-middle">
                             <thead>
                                 <tr>
-                                    
-                                    <th scope="col"><strong>Nombre</strong></th>
+                                    <th scope="col"><strong>Denominación</strong></th>
+                                    <th scope="col"><strong>Opciones</strong></th>
                                 </tr>
                             </thead>
                             <tbody id="tbPres">
@@ -106,9 +106,8 @@ include('../templates/cabecera.php');
                                     </select>
                                     <label for="exampleInputEmail1" class="mb-2">Presentación:</label>
                                     <button type="button" class="btn btn-outline-primary" data-coreui-toggle="modal" data-coreui-target="#presentacionModal"></button>
-                                    <select class="form-select form-select-sm" aria-label="Default select example" id="selUnidad" name="selUnidad">
-
-                                    </select>
+                                    <input type="text" class="form-control mb-2" id="selUnidad" name="selUnidad" placeholder="Ingrese Presentacion" data-coreui-toggle="modal" data-coreui-target="#presentacionModal" readonly>
+                                    
                                     <label for="exampleInputEmail1" class="mb-2">Cantidad:</label>
                                     <input type="text" class="form-control mb-2" id="ctdProducto" name="ctdProducto" placeholder="Ingrese cantidad">
                                     <label for="exampleInputEmail1" class="mb-2">Almacen:</label>
