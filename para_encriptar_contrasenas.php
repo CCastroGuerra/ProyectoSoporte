e3372b4f6c9c68d7178ddcb4f2f6b97d58cf4a8a
Frikortreat <87457741+Frikortreat@users.noreply.github.com>
2023-07-03 08:47:27 -0500

    Update para_encriptar_contrasenas.php

--- para_encriptar_contrasenas.php
+++ para_encriptar_contrasenas.php
@@ -1,25 +1,42 @@
 <?php
-   $pass="mypassword";
-$passmd5 = md5($pass);
-echo "passmd5= ", $passmd5,"<br>";
-$hashed_password = crypt($passmd5,$cryptoword);
-echo "hashed_password= ",$hashed_password,"<br>";
 
-$str = "mypasword";
+$pass="mypassword";
+$hash = encriptar($pass);
+echo "la contraseña es: ",$hash,"\n";
+echo "length: ",strlen($hash),"\n";
+$str = "mypassword";
 $user_input = md5($str);
-echo "user_input= ",$user_input,"<br>";
-echo "user_input= ",crypt($user_input,$cryptoword),"<br>";
+comprobar_pass($str,$hash);
+
+function encriptar($password){
+   $cryptoword ="Hospital_Las_Mercedes";
+   $passmd5 = md5($password);
+   //echo "passmd5= ", $passmd5,"<br>";
+   $hashed_password = crypt($passmd5,$cryptoword);
+   //echo "hashed_password= ",$hashed_password,"<br>";
+   return $hashed_password;
+}
+
+function comprobar_pass($user_input, $hashed_password){
+   if (hash_equals($hashed_password, crypt(md5($user_input), $hashed_password))) {
+      echo "¡Contraseña verificada!\n";
+      return true;
+   }
+   else{
+      echo "Contraseña erronea\n"; 
+      return false;
+   }
+}
+
+
+//echo "user_input= ",$user_input,"<br>";
+//echo "user_input= ",crypt($user_input,$cryptoword),"<br>";
 
 /* Se deben pasar todos los resultados de crypt() como el salt para la comparación de una
    contraseña, para evitar problemas cuando diferentes algoritmos hash son utilizados. (Como
    se dice arriba, el hash estándar basado en DES utiliza un salt de 2
    caracteres, pero el hash basado en MD5 utiliza 12.) */
 
-   if (hash_equals($hashed_password, crypt($user_input, $hashed_password))) {
-   echo "¡Contraseña verificada!";
-}
-else{
-	echo "Contraseña erronea"; 
-}
+
 
 ?>
