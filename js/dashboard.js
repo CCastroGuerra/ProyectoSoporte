a61fe35e0526a9eb9aa509e135d564cd99f1701a
Frikortreat <87457741+Frikortreat@users.noreply.github.com>
2023-07-14 13:08:10 -0500

    por subir

--- js/dashboard.js
+++ js/dashboard.js
@@ -26,11 +26,12 @@ function traerTrabajosxMes() {
     console.log(realizado);
     if (realizado == "") {
       //execute
-      console.log("vacio");
+      //console.log("vacio");
       mes.push(null);
       cantidad.push(null);
-      document.getElementById("miCanvas").setAttribute("style","display:none");
-      document.getElementById("no-data").style.display = "block";
+      /* document.getElementById("miCanvas").setAttribute("style", "display:none");
+      document.getElementById("no-data").style.display = "block"; */
+      
     } else {
       let respuesta = JSON.parse(realizado);
       console.log(respuesta);
@@ -43,16 +44,40 @@ function traerTrabajosxMes() {
         const myChart = new Chart(ctx, {
           type: "bar",
           data: {
-            labels: mes,
-            /* labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'], */
+            /*labels:  mes, */
+            labels: [
+              "Enero",
+              "Febrero",
+              "Marzo",
+              "Abril",
+              "Mayo",
+              "Junio",
+              "Julio",
+            ],
             datasets: [
               {
                 label: "Trabajos",
-                backgroundColor: colores,
+                backgroundColor: /* colores */ [
+                  generarColorAleatorio(),
+                  generarColorAleatorio(),
+                  generarColorAleatorio(),
+                  generarColorAleatorio(),
+                  generarColorAleatorio(),
+                  generarColorAleatorio(),
+                  generarColorAleatorio(),
+                ],
                 /* borderColor: coreui.Utils.getStyle('--cui-info'),
                 borderWidth: 2, */
-                data: cantidad,
-                /* data: [random(50, 200), random(50, 200), random(50, 200), random(50, 200), random(50, 200), random(50, 200), random(50, 200)], */
+                /* data: cantidad, */
+                data: [
+                  random(50, 200),
+                  random(50, 200),
+                  random(50, 200),
+                  random(50, 200),
+                  random(50, 200),
+                  random(50, 200),
+                  random(50, 200),
+                ],
                 fill: false,
               },
             ],
@@ -104,23 +129,42 @@ function traerTrabajosxMes() {
                   dataSum = firstSet.reduce(
                     (accumulator, currentValue) => accumulator + currentValue
                   );
-                console.log("firstSet: "+firstSet);
-                console.log("datasum: "+dataSum);
-    
-                if (typeof firstSet !== "object" || dataSum === 0) {
+                console.log("firstSet: " + firstSet);
+                console.log("datasum: " + dataSum);
+
+                if (dataSum === 0) {
                   console.log("se cumple ocultando");
                   document.getElementById("no-data").style.display = "block";
-                  document.getElementById("miCanvas").style.display =
-                    "none";
+                  document.getElementById("miCanvas").style.display = "none";
                 }
               },
             },
           },
         });
+        var firstSet = myChart.config.data.datasets[0].data;
+        var ejex = myChart.config.data.labels;
+        var dataSum = firstSet.reduce(
+          (accumulator, currentValue) => accumulator + currentValue
+        );
+        var colrs = myChart.config.data.datasets[0].backgroundColor;
+        var template="";
+        console.log(dataSum);
+        var foot= document.getElementById("Foot1-content");
+        for (let index = 0; index < ejex.length; index++) {
+          var perc= ((firstSet[index]/dataSum)*100).toFixed(2);
+          console.log("(" + ejex[index] + "," + firstSet[index] + ")=> "+perc+"% " +"c:"+colrs[index]);
+          
+          template += `<div class="col mb-sm-2 mb-0">
+          <div class="text-medium-emphasis">Visits</div>
+          <div class="fw-semibold">${firstSet[index]} ${ejex[index]} (${perc}%)</div>
+          <div class="progress progress-thin mt-2">
+            <div class="progress-bar" role="progressbar" style="width: ${perc}%; background-color:${colrs[index]}" aria-valuenow="${perc}" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>`;
+        }
+        foot.innerHTML = template;
       }
     }
-
-   
   };
   ajax.send(data);
 }
@@ -136,64 +180,95 @@ function traerTrabajosXArea() {
     let area = [];
     let cantidad2 = [];
     realizado = ajax.responseText;
-    let respuesta = JSON.parse(realizado);
-    //console.log(respuesta);
-    for (let i = 0; i < respuesta.length; i++) {
-      area.push(respuesta[i].area);
-      cantidad2.push(respuesta[i].cantidad);
-      colores.push(generarColorAleatorio());
-    }
-    const canva = document.getElementById("miCanvas2");
-    let colors = ["#44FF07", "#FED60A", "#FB0007", "#3700FF", "#FB13F3"];
-    const myChart2 = new Chart(canva, {
-      type: "pie",
-      data: {
-        labels: area,
-        datasets: [
-          {
-            label: "",
-            data: cantidad2,
-            backgroundColor: colores,
-            /* borderColor: ["rgba(1,5,22,1)"],
+    if (realizado == "") {
+      //execute
+      console.log("vacio");
+      mes.push(null);
+      cantidad.push(null);
+      document.getElementById("miCanvas").setAttribute("style", "display:none");
+      document.getElementById("no-data2").style.display = "block";
+    } else {
+      let respuesta = JSON.parse(realizado);
+      //console.log(respuesta);
+      for (let i = 0; i < respuesta.length; i++) {
+        area.push(respuesta[i].area);
+        cantidad2.push(respuesta[i].cantidad);
+        colores.push(generarColorAleatorio());
+      }
+      const canva = document.getElementById("miCanvas2");
+      let colors = ["#44FF07", "#FED60A", "#FB0007", "#3700FF", "#FB13F3"];
+      const myChart2 = new Chart(canva, {
+        type: "pie",
+        data: {
+          labels: area,
+          datasets: [
+            {
+              label: "",
+              data: cantidad2,
+              backgroundColor: colores,
+              /* borderColor: ["rgba(1,5,22,1)"],
             borderWidth: 2, */
+            },
+          ],
+        },
+        options: {
+          responsive: true, // Hace que el gráfico sea responsive
+          maintainAspectRatio: false,
+          // Permite ajustar el tamaño del gráfico
+          //indexAxis: "y", //cambiar de posicion el grafico
+          scales: {
+            x: {
+              grid: {
+                display: false,
+                drawOnChartArea: false,
+                drawBorder: false,
+              },
+              ticks: {
+                display: false,
+              },
+            },
+            y: {
+              beginAtZero: true,
+              grid: {
+                display: false,
+              },
+              ticks: {
+                display: false,
+              },
+            },
           },
-        ],
-      },
-      options: {
-        responsive: true, // Hace que el gráfico sea responsive
-        maintainAspectRatio: false,
-        // Permite ajustar el tamaño del gráfico
-        //indexAxis: "y", //cambiar de posicion el grafico
-        scales: {
-          x: {
-            grid: {
-              display: false,
-              drawOnChartArea: false,
-              drawBorder: false,
-            },
-            ticks: {
-              display: false,
-            },
-          },
-          y: {
-            beginAtZero: true,
-            grid: {
-              display: false,
-            },
-            ticks: {
-              display: false,
+          plugins: {
+            tooltip: {
+              enabled: true,
+              external: null,
+              position: "average",
             },
           },
         },
-        plugins: {
-          tooltip: {
-            enabled: true,
-            external: null,
-            position: "average",
-          },
-        },
-      },
-    });
+      });
+      var firstSet = myChart2.config.data.datasets[0].data;
+        var ejex = myChart2.config.data.labels;
+        var dataSum = firstSet.reduce(
+          (accumulator, currentValue) => accumulator + currentValue
+        );
+        var colrs = myChart2.config.data.datasets[0].backgroundColor;
+        var template="";
+        console.log(dataSum);
+        var foot= document.getElementById("Foot2-content");
+        for (let index = 0; index < ejex.length; index++) {
+          var perc= ((firstSet[index]/dataSum)*100).toFixed(2);
+          console.log("(" + ejex[index] + "," + firstSet[index] + ")=> "+perc+"% " +"c:"+colrs[index]);
+          
+          template += `<div class="col mb-sm-2 mb-0">
+          <div class="text-medium-emphasis">Visits</div>
+          <div class="fw-semibold">${firstSet[index]} ${ejex[index]} (${perc}%)</div>
+          <div class="progress progress-thin mt-2">
+            <div class="progress-bar" role="progressbar" style="width: ${perc}%; background-color:${colrs[index]}" aria-valuenow="${perc}" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        </div>`;
+        }
+        foot.innerHTML = template;
+    }
   };
   ajax.send(data);
 }
--- templates/footer.php
+++ templates/footer.php
@@ -3,8 +3,9 @@
 </div>
 </div>
 <footer class="footer">
-</footer>
-</div>
+        <div><a href="https://coreui.io">CoreUI </a><a href="https://coreui.io">Bootstrap Admin Template</a> © 2022 creativeLabs.</div>
+        <div class="ms-auto">Powered by&nbsp;<a href="https://coreui.io/docs/">CoreUI UI Components</a></div>
+      </footer>
 <!-- limpiar form -->
 <script src="../js/limpiarForm.js"></script>
 
